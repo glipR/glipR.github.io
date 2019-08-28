9190c56af08277509301aefe3c9b49888465ba6c
glipR <jgoe0001@student.monash.edu>
2019-08-28 10:13:14 +1000

    Alter title and description.

--- source/posts/Pathfinding/Intro/info.py
+++ source/posts/Pathfinding/Intro/info.py
@@ -2,7 +2,7 @@ from objects import Post as BasePost
 
 class Post(BasePost):
 
-    title = 'Pathfinding'
-    description = 'How computers get you from A to B'
+    title = 'Computers from A to B'
+    description = 'An introduction and Natural Search Algorithms'
 
-    url = title + '/' + 'Intro'
+    url = 'Pathfinding/Intro'
