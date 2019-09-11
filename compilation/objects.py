57bfc606f4faed4dd1f981614b358c1a73e0cad6
glipR <jgoe0001@student.monash.edu>
2019-09-11 17:30:32 +1000

    Add updated time to post.

--- compilation/objects.py
+++ compilation/objects.py
@@ -1,8 +1,15 @@
+from datetime import date
+
 class Post(object):
 
     title = 'blog.glipr.dev'
     description = 'What goes here lol'
 
+    last_updated = date.today()
+
     @property
     def url(self):
         return self.title
+
+    def get_last_updated(self):
+        return self.last_updated.strftime('%B %d, %Y')
--- source/posts/Pathfinding/Intro/info.py
+++ source/posts/Pathfinding/Intro/info.py
@@ -1,4 +1,5 @@
 from objects import Post as BasePost
+from datetime import date
 
 class Post(BasePost):
 
@@ -6,3 +7,5 @@ class Post(BasePost):
     description = 'An introduction and Natural Search Algorithms'
 
     url = 'Pathfinding/Intro'
+
+    last_updated = date(2019, 9, 11)
