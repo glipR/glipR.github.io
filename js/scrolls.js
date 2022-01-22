af61450724fb2e32bf2e69351704ca9a122e0dce
glipR <37640160+glipR@users.noreply.github.com>
2022-01-22 21:49:31 +1100

    Hide pages not loaded.

--- js/scrolls.js
+++ js/scrolls.js
@@ -1,6 +1,12 @@
-function setScrolls() {
+function setScrolls(show) {
     const el = document.getElementsByClassName("toppanel-content");
     for (var i=0; i<el.length; i++) {
         el[i].scrollTo(0, 0);
+        if (el[i].parentElement.id == show) {
+            el[i].parentElement.hidden = false;
+        } else {
+            el[i].parentElement.hidden = true;
+        }
     }
 }
+window.onload = ()=>{setScrolls("nothing")};
