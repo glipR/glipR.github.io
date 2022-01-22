0c49aec49efe96917e3af98496ef1ca197438d9f
glipR <37640160+glipR@users.noreply.github.com>
2022-01-22 22:22:58 +1100

    Fix mobile scrolling.

--- js/scrolls.js
+++ js/scrolls.js
@@ -1,12 +1,14 @@
 function setScrolls(show) {
     const el = document.getElementsByClassName("toppanel-content");
     for (var i=0; i<el.length; i++) {
-        el[i].scrollTo(0, 0);
         if (el[i].parentElement.id == show) {
             el[i].parentElement.hidden = false;
         } else {
             el[i].parentElement.hidden = true;
         }
+    }
+    for (var i=0; i<el.length; i++) {
+        el[i].scrollTo(0, 0);
     }
 }
 window.onload = ()=>{setScrolls("nothing")};
